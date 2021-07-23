c77386ef97cd7cd5230147ff10656b247d8aedaa
nao-18 <kyahyo17@yahoo.co.jp>
2021-07-23 22:52:35 +0900

    slotは複数同時に使える

--- lesson24/src/src/components/LikeHeader.vue
+++ lesson24/src/src/components/LikeHeader.vue
@@ -3,6 +3,14 @@
     <!-- slotは全てのtemplateを送られる -->
     <!-- slotタグ内の記述は全てdefault値として扱われる(フォールバックコンテンツ) -->
     <slot>デフォルトのタイトル</slot>
+    <hr />
+
+    <!--
+      複数slotを同時に使える
+     -->
+    <slot></slot>
+    <slot></slot>
+    <slot></slot>
   </div>
 </template>
 
