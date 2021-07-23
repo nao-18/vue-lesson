df9fbdb06852738d4c7d209656370df5d643b548
nao-18 <kyahyo17@yahoo.co.jp>
2021-07-23 22:46:04 +0900

    slotによって親から子へtemplateを送れる

--- lesson24/src/src/components/LikeHeader.vue
+++ lesson24/src/src/components/LikeHeader.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>トータルのいいね</h1>
+  <div>
+    <!-- slotは全てのtemplateを送れる -->
+    <slot></slot>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ["headerText"],
+};
+</script>
