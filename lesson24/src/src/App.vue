5ec3d1a496e499ab23b0e9f9dff94367e171532c
nao-18 <kyahyo17@yahoo.co.jp>
2021-07-24 18:56:43 +0900

    複数checkboxでv-modelを使う場合は配列でdataを設定する

--- lesson24/src/src/App.vue
+++ lesson24/src/src/App.vue
@@ -21,21 +21,29 @@
         v-model.number="eventData.maxNumber"
       />
       <p>{{ eventData.maxNumber }}</p>
+      <label for="host">主催者</label>
+      <input id="host" type="text" v-model.trim="eventData.host" />
+      <pre>{{ eventData.host }}</pre>
+      <label for="detail">イベントの内容</label>
+      <textarea
+        id="detail"
+        cols="30"
+        rows="10"
+        v-model="eventData.detail"
+      ></textarea>
+      <p style="white-space: pre">{{ eventData.detail }}</p>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate" />
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate }}</p>
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target" />
+      <label for="10">10代</label>
+      <input type="checkbox" id="20" value="20代" v-model="eventData.target" />
+      <label for="20">20代</label>
+      <input type="checkbox" id="30" value="30代" v-model="eventData.target" />
+      <label for="30">10代</label>
+      <p>{{ eventData.target }}</p>
     </div>
-    <label for="host">主催者</label>
-    <input id="host" type="text" v-model.trim="eventData.host" />
-    <pre>{{ eventData.host }}</pre>
-    <label for="detail">イベントの内容</label>
-    <textarea
-      id="detail"
-      cols="30"
-      rows="10"
-      v-model="eventData.detail"
-    ></textarea>
-    <p style="white-space: pre fdsa">{{ eventData.detail }}</p>
-    <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate" />
-    <label for="isPrivate">非公開</label>
-    <p>{{ eventData.isPrivate }}</p>
   </div>
 </template>
 
@@ -56,6 +64,7 @@ export default {
         host: "",
         detail: "",
         isPrivate: false,
+        target: [],
       },
     };
   },
