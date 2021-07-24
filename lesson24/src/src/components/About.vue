b4ad10629dea0c7bf0962d77319bf21c9856ec85
nao-18 <kyahyo17@yahoo.co.jp>
2021-07-24 18:01:35 +0900

    keep-aliveを使用する場合にactivatedとdeactivatedの2つのライフサイクルが追加される

--- lesson24/src/src/components/About.vue
+++ lesson24/src/src/components/About.vue
@@ -4,3 +4,16 @@
     <input type="text" />
   </div>
 </template>
+
+<script>
+export default {
+  activated() {
+    //keep-aliveにてコンポーネントが選択状態で呼び出される。
+    console.log("Activated");
+  },
+  deactivated() {
+    //keep-aliveにてコンポーネントの選択状態が解除された際に呼び出される。
+    console.log("Deactivated");
+  },
+};
+</script>
